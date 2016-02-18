628279b49c9b75c22a870e2515b0a7e1abf0c102
Luke Todd <lukejamestodd1@gmail.com>
2016-02-18 14:51:26 +1100

    contacts pages half-working

--- javascripts/router.js
+++ javascripts/router.js
@@ -30,14 +30,14 @@ var Router = Backbone.Router.extend({
 	routes: {
 		"": "showIndex",
 		"login": "login",
-		// "user/new": "signUp",
+		// "signup": "signUp",
     //
 		// "trainer/dash": "trainerDash",
 		// "trainer/activities": "trainerActivitiesList",
 		// "trainer/activities/:id": "trainerActivityDetails",
 		// "trainer/activities/new": "trainerActivityCreate",
 		// "trainer/activities/edit": "trainerActivityEdit",
-		// "trainer/contacts": "trainerContactsList",
+		"trainer/contacts": "trainerContactsList",
 		// "trainer/contacts/message": "trainerSendMessage",
 		// "trainer/calendar": "trainerCalendar",
     //
@@ -45,26 +45,8 @@ var Router = Backbone.Router.extend({
 		// "student/activities": "studentActivitiesList",
 		// "student/activities/:id": "studentActivityDetails",
 		// "student/activities/:id/book": "studentActivityBook",
-		// "student/contacts": "studentContactsList",
-		// "student/contacts/:id": "studentTrainerDetail",
-		// "student/calendar": "studentCalendar"
-		// "user/new": "signUp",
-    //
-		// "trainer/dash": "trainerDash",
-		// "trainer/activities": "trainerActivitiesList",
-		// "trainer/activities/:id": "trainerActivityDetails",
-		// "trainer/activities/new": "trainerActivityCreate",
-		// "trainer/activities/edit": "trainerActivityEdit",
-		// "trainer/contacts": "trainerContactsList",
-		// "trainer/contacts/message": "trainerSendMessage",
-		// "trainer/calendar": "trainerCalendar",
-    //
-		// "student/dash": "studentDash",
-		// "student/activities": "studentActivitiesList",
-		// "student/activities/:id": "studentActivityDetails",
-		// "student/activities/:id/book": "studentActivityBook",
-		// "student/contacts": "studentContactsList",
-		// "student/contacts/:id": "studentTrainerDetail",
+		"student/contacts": "studentContactsList",
+		//	"student/contacts/:id": "studentTrainerDetail",
 		// "student/calendar": "studentCalendar"
 	},
 
@@ -85,122 +67,6 @@ var Router = Backbone.Router.extend({
 
     var footer = new FooterView();
     $('body').append(footer.render().el);
-
-		// //show student dashboard
-		// var studentDash = new StudentDashboard();
-		// $('body').append(studentDash.render().el);
-
-		//shows all users
-		// var users = new Users();
-		// users.fetch().done(function() {
-		// 	users.each(function(person) {
-		// 		var view = new UserItemView({ model: person});
-		// 		$('.users-list').append(view.render().el);
-		// 	});
-		// });
-
-		//shows all activities
-		// //shows all activities
-		// var activities = new Activities();
-		// activities.fetch().done(function(){
-		// 	activities.each(function(instance){
-		// 		var view = new ActivityItemView({ model: instance});
-		// 		$('.activities-list').append(view.render().el);
-		// 	});
-		// });
-    //
-		// //shows one activity
-		// // console.log('index page function');
-		// // var activity = new Activity({id: 11});
-		// // activity.fetch().done(function(){
-		// // 	//make a view showing the activity
-		// // 	var activityView = new ActivityDetailView({model: activity});
-		// // 	$('.activity-detail').append(activityView.render().el);
-		// // });
-    //
-		// //show one activity plus trainer info,
-		// //venue info, participants list
-		// //change from hard coded when more data exists in DB
-		// console.log('SHOW PAGE');
-		// var activity = new Activity({id: 22});
-
-		//shows one activity
-		// console.log('index page function');
-		// var activity = new Activity({id: 11});
-		// activity.fetch().done(function(){
-		// 	//make a view showing the activity
-		// 	var activityView = new ActivityDetailView({model: activity});
-		// 	$('.activity-detail').append(activityView.render().el);
-    //
-		// 	var instructor = new User({id: activity.get('user_id')});
-		// 	instructor.fetch().done(function(){
-		// 		var instructorView = new UserItemView({model: instructor});
-		// 		$('.user-detail').append(instructorView.render().el);
-    //
-		// 		//need more venue data in DB for this to work
-		// 		var venue = new Venue({id: activity.get('venue_id')});
-		// 		venue.fetch().done(function(){
-		// 			var venueView = new VenueDetailView({model: venue});
-		// 			$('.venue-detail').append(venueView.render().el);
-    //
-		// 			//need more data in DB for this to work
-		// 			var activityType = new ActivityType({id: activity.get('activity_type_id')});
-		// 			activityType.fetch().done(function(){
-		// 				var activityTypeView = new ActivityTypeDetailView({model: activityType});
-		// 				$('.activity-type-detail').append(activityTypeView.render().el);
-    //
-		// 				//shows all participants with this activity id
-		// 				var participants = new Participants({activity_id: activity.get('activity_id')});
-		// 				participants.fetch().done(function() {
-		// 					participants.each(function(attendee) {
-		// 						var view = new ParticipantItemView({ model: attendee});
-		// 						$('.participants-list').append(view.render().el);
-		// 					});
-		// 				});
-		// 			});
-		// 		});
-		// 	});
-		// });
-
-		//show one activity plus trainer info,
-		//venue info, participants list
-		//change from hard coded when more data exists in DB
-		// console.log('SHOW PAGE');
-		// var activity = new Activity({id: 22});
-		// activity.fetch().done(function(){
-		// 	//make a view showing the activity
-		// 	var activityView = new ActivityDetailView({model: activity});
-		// 	$('.activity-detail').append(activityView.render().el);
-
-		// 	var instructor = new User({id: activity.get('user_id')});
-		// 	instructor.fetch().done(function(){
-		// 		var instructorView = new UserItemView({model: instructor});
-		// 		$('.user-detail').append(instructorView.render().el);
-
-		// 		//need more venue data in DB for this to work
-		// 		var venue = new Venue({id: activity.get('venue_id')});
-		// 		venue.fetch().done(function(){
-		// 			var venueView = new VenueDetailView({model: venue});
-		// 			$('.venue-detail').append(venueView.render().el);
-
-		// 			//need more data in DB for this to work
-		// 			var activityType = new ActivityType({id: activity.get('activity_type_id')});
-		// 			activityType.fetch().done(function(){
-		// 				var activityTypeView = new ActivityTypeDetailView({model: activityType});
-		// 				$('.activity-type-detail').append(activityTypeView.render().el);
-
-		// 				//shows all participants with this activity id
-		// 				var participants = new Participants({activity_id: activity.get('activity_id')});
-		// 				participants.fetch().done(function() {
-		// 					participants.each(function(attendee) {
-		// 						var view = new ParticipantItemView({ model: attendee});
-		// 						$('.participants-list').append(view.render().el);
-		// 					});
-		// 				});
-		// 			});
-		// 		});
-		// 	});
-		// });
 	},
 
 	login: function(){
@@ -241,15 +107,16 @@ var Router = Backbone.Router.extend({
 	},
 
 	trainerContactsList: function(){
+		setupBody();
 		//container for contacts list
-		var contactsListContainer = new ContactsListContainer();
-		$('body').append(contactsListContainer.render().el);
+		var contactListContainer = new ContactListContainerView();
+		$('body').append(contactListContainer.render().el);
 
 		//contacts list with user data
 		var contacts = new Contacts();
 		contacts.fetch().done(function() {
 			contacts.each(function(person) {
-				var contactsList = new UserItemView({ model: person});
+				var contactsList = new ContactItemView({ model: person});
 				$('.users-list').append(contactsList.render().el);
 			});
 		});
@@ -280,6 +147,7 @@ var Router = Backbone.Router.extend({
 	},
 
 	studentContactsList: function(){
+		setupBody();
 		//container for contacts list
 		var trainersListContainer = new TrainersListContainer();
 		$('body').append(trainersListContainer.render().el);
--- javascripts/views.js
+++ javascripts/views.js
@@ -55,255 +55,37 @@ var LoginFormView = Backbone.View.extend({
   }
 });
 
-// var IndexView = Backbone.View.extend({
-// 	tagName: 'div',
-// 	className: 'container',
-// 	template: $('#index-template').html(),
-// 	render: function(){
-// 		var html = Mustache.render(this.template, this.model.toJSON());
-// 		this.$el.html(html);
-// 		return this;
-// 	}
-// });
-//
-//
-//
-// var LoginView = Backbone.View.extend({
-// 	tagName: 'div',
-// 	className: 'container',
-// 	template: $('#login-template').html(),
-// 	render: function(){
-// 		var html = Mustache.render(this.template, this.model.toJSON());
-// 		this.$el.html(html);
-// 		return this;
-// 	}
-// });
-//
-// var SignUpView = Backbone.View.extend({
-// 	tagName: 'div',
-// 	className: 'container',
-// 	template: $('#signup-template').html(),
-// 	render: function(){
-// 		var html = Mustache.render(this.template, this.model.toJSON());
-// 		this.$el.html(html);
-// 		return this;
-// 	}
-// });
-//
-//
-//
-//
-// //========== PAGE SECTION VIEWS ===========//
-//
-//
-// //for student dashboard
-// var StudentDashboard = Backbone.View.extend({
-// 	tagName: 'div',
-// 	className: 'dash-title',
-// 	template: $('#student-template').html(),
-// 	render: function(){
-// 		var html = Mustache.render(this.template);
-// 		this.$el.html(html);
-// 		return this;
-// 	}
-// });
-//
-// //for trainer contacts list container
-// var ContactsListContainer = Backbone.View.extend({
-// 	tagName: 'div',
-// 	className: 'container',
-// 	template: $('#trainer-contacts-list-template').html(),
-// 	render: function(){
-// 		var html = Mustache.render(this.template);
-// 		this.$el.html(html);
-// 		return this;
-// 	}
-// });
-//
-// //for student contacts list container
-// var TrainersListContainer = Backbone.View.extend({
-// 	tagName: 'div',
-// 	className: 'container',
-// 	template: $('#student-contacts-list-template').html(),
-// 	render: function(){
-// 		var html = Mustache.render(this.template);
-// 		this.$el.html(html);
-// 		return this;
-// 	}
-// });
-//
-// //for a list of users
-// var UserItemView = Backbone.View.extend({
-// 	tagName: 'tr',
-// 	// className: '',
-// 	template: $('#user-item-template').html(),
-// 	render: function() {
-// 		var html = Mustache.render(this.template, this.model.toJSON());
-// 		this.$el.html(html);
-// 		return this;
-// 	}
-// });
-//
-// //for list of activities on main page
-// var ActivityItemView = Backbone.View.extend({
-// 	tagName: 'div',
-// 	className: 'ui-card',
-// 	template: $('#activity-item-template').html(),
-// 	render: function() {
-// 		var html = Mustache.render(this.template, this.model.toJSON());
-// 		this.$el.html(html);
-// 		return this;
-// 	}
-// });
-//
-// //for detailed view of a single activity
-// var ActivityDetailView = Backbone.View.extend({
-// 	tagName: 'div',
-// 	className: 'container',
-// 	template: $('#activity-detail-view').html(),
-// 	render: function(){
-// 		var html = Mustache.render(this.template, this.model.toJSON());
-// 		this.$el.html(html);
-// 		return this;
-// 	}
-// });
-//
-// //for "show activity" page - venue info
-// var VenueDetailView = Backbone.View.extend({
-// 	tagName: 'div',
-// 	className: 'container',
-// 	template: $('#venue-item-template').html(),
-// 	render: function(){
-// 		var html = Mustache.render(this.template, this.model.toJSON());
-// 		this.$el.html(html);
-// 		return this;
-// 	}
-// });
-//
-// //for "show activity" page - activity type (to get name)
-// var ActivityTypeDetailView = Backbone.View.extend({
-// 	tagName: 'div',
-// 	className: 'container',
-// 	template: $('#activity-detail-item-template').html(),
-// 	render: function(){
-// 		var html = Mustache.render(this.template, this.model.toJSON());
-// 		this.$el.html(html);
-// 		return this;
-// 	}
-// });
-//
-// //for "show activity" page - list of participants
-// var ParticipantItemView = Backbone.View.extend({
-// 	tagName: 'div',
-// 	className: 'container',
-// 	template: $('#participant-item-template').html(),
-// 	render: function() {
-// 		var html = Mustache.render(this.template, this.model.toJSON());
-// 		this.$el.html(html);
-// 		return this;
-// 	}
-// });
-//
-// //show all the ui-cards by appending them to "list" on page
-// // users.each(function(person) {
-// // 		console.log(person.get('email'));
-// // 		var view = new ObjectItemView({ model: person, template: $userTemplate});
-// // 		$('main').append(view.render().el);
-// // });
-//
-// // venues.each(function(place) {
-// // 		console.log(place.get('address'));
-// // 		var view = new ObjectItemView({ model: place, template:  $venueTemplate});
-// // 		$('.list').append(view.render().el);
-// // });
-//
-// // participants.each(function(attendee) {
-// // 		console.log(attendee.get('user_id'));
-// // 		var view = new ObjectItemView({ model: attendee, template:  $participantTemplate});
-// // 		$('.list').append(view.render().el);
-// // });
-//
-// // activityTypes.each(function(typeOfActivity) {
-// // 		console.log(typeOfActivity.get('description'));
-// // 		var view = new ObjectItemView({ model: typeOfActivity, template:  $activityTypeTemplate});
-// // 		$('.list').append(view.render().el);
-// // });
-//
-// // contacts.each(function(client) {
-// // 		console.log(client.get('created_at'));
-// // 		var view = new ObjectItemView({ model: client, template:  $contactTemplate});
-// // 		$('.list').append(view.render().el);
-// // });
-//
-// // activities.each(function(instance) {
-// // 		console.log(instance.get('title'));
-// // 		var view = new ObjectItemView({ model: instance, template:  $activityTemplate});
-// // 		$('.list').append(view.render().el);
-// // });
-//
-// // groups.each(function(club) {
-// // 		console.log(club.get('name'));
-// // 		var view = new ObjectItemView({ model: club, template:  $groupTemplate});
-// // 		$('.list').append(view.render().el);
-// // });
-//
-// // groupEntries.each(function(ge) {
-// // 		console.log(ge.get('group_id'));
-// // 		var view = new ObjectItemView({ model: ge, template:  $groupEntriesTemplate});
-// // 		$('.list').append(view.render().el);
-// // });
-//
-//
-// /*
-//
-// //========== temporary arrays to store new backbone objects
-//
-// var usersArray = [];
-// var activitiesArray = [];
-// var venuesArray = [];
-// var participantsArray = [];
-// var activityTypesArray = [];
-//
-//
-// //=====create objects for each element in each api class
-// //and store in the appropriate objects array
-//
-// var makeObjects = function(objectModel, apiCollection, objectsArray){
-// 	for (var i = 0; i < apiCollection.length; i++){
-// 		var object = new objectModel();
-//
-// 		//for each key in the api element, make an attribute for the Backbone object
-//
-// 		//set the value for each attribute to the value in api data. eg:
-// 		object.set({
-// 			id: i,
-// 			name: apiData[i].name,
-// 			ability: apiData[i].ability,
-// 			phrase: apiData[i].phrase,
-// 			image_url: apiData[i].image_url
-// 		});
-// 		console.log(mutant.get('name'));
-// 		objectsArray.push(object);
-// 	}
-// }
-//
-// makeObects(User, apiData[index], usersArray);
-// makeObects(Activity, apiData[index], activitiesArray);
-// makeObects(Venue, apiData[index], venuesArray);
-// makeObects(Participant, apiData[index], participantsArray);
-// makeObects(ActivityType, apiData[index], activityTypesArray);
-//
-//
-// //===== make new instance of each collection by passing in  js array of objects
-// var users = new Users(usersArray);
-// var activites = new Activities(activitiesArray);
-// var venues = new Venues(venuesArray);
-// var participants = new Participants(participantsArray);
-// var activityTypes = new ActivitiesTypes(activityTypesArray);
-//
-//
-//
-//
-//
-//
-// */
+//for trainer contacts list container
+var ContactListContainerView = Backbone.View.extend({
+  tagName: 'div',
+  className: 'container',
+  template: $('#trainer-contacts-list-template').html(),
+  render: function(){
+    var html = Mustache.render(this.template);
+    this.$el.html(html);
+    return this;
+  }
+});
+
+//for student contacts list container
+var TrainersListContainer = Backbone.View.extend({
+  tagName: 'div',
+  className: 'container',
+  template: $('#student-contacts-list-template').html(),
+  render: function(){
+    var html = Mustache.render(this.template);
+    this.$el.html(html);
+    return this;
+  }
+});
+
+//for a list of users
+var ContactItemView = Backbone.View.extend({
+  tagName: 'tr',
+  template: $('#user-item-template').html(),
+  render: function() {
+    var html = Mustache.render(this.template, this.model.toJSON());
+    this.$el.html(html);
+    return this;
+  }
+});
